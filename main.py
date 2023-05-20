9f08150b53651c9dfe039b2520616594103a31b6
Viktorre <reif.viktor@gmail.com>
2023-05-20 19:07:24 +0200

    updated readme. restrucutred main and utils.

--- main.py
+++ main.py
@@ -1,149 +1,11 @@
-"""A graph path discovery coding task.
+from typing import List
 
-In this file, you are presented with the task to implement the function `compute_shortest_paths`
-which discovers some shortest paths from a start node to an end node in an undirected weighted graph
-with strictly positive edge lengths. The function is marked with "TODO: Write" below and carries a
-more precise specification of the expected behavior in its docstring.
-
-Please write the implementation with the highest quality standards in mind which you would also use
-for production code. Functional correctness is the most important criterion. After that it will be
-evaluated in terms of maintainability and wall clock runtime for large graphs (in decreasing order
-of importance). Please submit everything you have written, including documentation and tests.
-
-Your implementation of `compute_shortest_paths` should target Python 3.9 and not use any external
-dependency except for the Python standard library. Outside of the implementation of
-`compute_shortest_paths` itself, you are free to use supporting libraries as long as they are
-available on PyPi.org. If you use additional packages, please add a requirements.txt file which
-lists them with their precise versions ("packageA==1.2.3").
-"""
-from functools import total_ordering
-from typing import Any, List, Optional, List, Tuple, cast
-
-
-class Node:
-    """A node in a graph."""
-
-    def __init__(self, id: int):
-        self.id: int = id
-        self.adjacent_edges: List["UndirectedEdge"] = []
-
-    def edge_to(self, other: "Node") -> Optional["UndirectedEdge"]:
-        """Returns the edge between the current node and the given one (if existing)."""
-        matches = [
-            edge for edge in self.adjacent_edges if edge.other_end(self) == other
-        ]
-        return matches[0] if len(matches) > 0 else None
-
-    def is_adjacent(self, other: "Node") -> bool:
-        """Returns whether there is an edge between the current node and the given one."""
-        return other in {edge.other_end(self) for edge in self.adjacent_edges}
-
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, Node) and self.id == other.id
-
-    def __le__(self, other: Any) -> bool:
-        return isinstance(other, Node) and self.id <= other.id
-
-    def __hash__(self) -> int:
-        return self.id
-
-    def __repr__(self) -> str:
-        return f"Node({self.id})"
-
-
-class UndirectedEdge:
-    """An undirected edge in a graph."""
-
-    def __init__(self, end_nodes: Tuple[Node, Node], length: float):
-        self.end_nodes: Tuple[Node, Node] = end_nodes
-        if 0 < length:
-            self.length: float = length
-        else:
-            raise ValueError(
-                f"Edge connecting {end_nodes[0].id} and {end_nodes[1].id}: "
-                f"Non-positive length {length} not supported."
-            )
-
-        if any(
-            e.other_end(end_nodes[0]) == end_nodes[1]
-            for e in end_nodes[0].adjacent_edges
-        ):
-            raise ValueError("Duplicate edges are not supported")
-
-        self.end_nodes[0].adjacent_edges.append(self)
-        if self.end_nodes[0] != self.end_nodes[1]:
-            self.end_nodes[1].adjacent_edges.append(self)
-        self.end_node_set = set(self.end_nodes)
-
-    def other_end(self, start: Node) -> Node:
-        """Returns the other end of the edge, given one of the end nodes."""
-        return self.end_nodes[0] if self.end_nodes[1] == start else self.end_nodes[1]
-
-    def is_adjacent(self, other_edge: "UndirectedEdge") -> bool:
-        """Returns whether the current edge shares an end node with the given edge."""
-        return len(self.end_node_set.intersection(other_edge.end_node_set)) > 0
-
-    def __repr__(self) -> str:
-        return (
-            f"UndirectonalEdge(({self.end_nodes[0].__repr__()}, "
-            f"{self.end_nodes[1].__repr__()}), {self.length})"
-        )
-
-
-class UndirectedGraph:
-    """A simple undirected graph with edges attributed with their length."""
-
-    def __init__(self, edges: List[UndirectedEdge]):
-        self.edges: List[UndirectedEdge] = edges
-        self.nodes_by_id = {
-            node.id: node for edge in self.edges for node in edge.end_nodes
-        }
-
-
-@total_ordering
-class UndirectedPath:
-    """An undirected path through a given graph."""
-
-    def __init__(self, nodes: List[Node]):
-        assert all(
-            node_1.is_adjacent(node_2) for node_1, node_2 in zip(nodes[:-1], nodes[1:])
-        ), "Path edges must be a chain of adjacent nodes"
-        self.nodes: List[Node] = nodes
-        self.length = sum(
-            cast(UndirectedEdge, node_1.edge_to(node_2)).length
-            for node_1, node_2 in zip(nodes[:-1], nodes[1:])
-        )
-
-    @property
-    def start(self) -> Node:
-        return self.nodes[0]
-
-    @property
-    def end(self) -> Node:
-        return self.nodes[-1]
-
-    def prepend(self, edge: UndirectedEdge) -> "UndirectedPath":
-        if self.start not in edge.end_nodes:
-            raise ValueError("Edge is not adjacent")
-        return UndirectedPath([edge.other_end(self.start)] + self.nodes)
-
-    def append(self, edge: UndirectedEdge) -> "UndirectedPath":
-        if self.end not in edge.end_nodes:
-            raise ValueError("Edge is not adjacent")
-        return UndirectedPath(self.nodes + [edge.other_end(self.end)])
-
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, UndirectedPath) and self.nodes == other.nodes
-
-    def __le__(self, other: Any) -> bool:
-        return isinstance(other, UndirectedPath) and self.length <= other.length
-
-    def __hash__(self) -> int:
-        return hash(n.id for n in self.nodes)
-
-    def __repr__(self) -> str:
-        nodestr: str = ", ".join([node.__repr__() for node in self.nodes])
-        return f"UndirectedPath([{nodestr}])"
+from utils import (
+    UndirectedEdge,
+    UndirectedGraph,
+    UndirectedPath,
+    Node,
+)
 
 
 def compute_shortest_paths(
@@ -172,7 +34,7 @@ def compute_shortest_paths(
         )
         paths = remove_paths_that_are_too_long(paths, length_tolerance)
         arrived_paths = remove_paths_that_are_too_long(arrived_paths, length_tolerance)
-        
+
     return sorted(arrived_paths, key=lambda path: path.length, reverse=False)
 
 
@@ -244,5 +106,3 @@ if __name__ == "__main__":
 
     # Should print the paths [1, 2, 4], [1, 3, 4], [1, 2, 4, 2, 4], [1, 2, 1, 2, 4], [1, 2, 4, 3, 4]
     print(compute_shortest_paths(demo_graph, n1, n4, 2.0))
-
-    # print(len(compute_shortest_paths(demo_graph, n1, n4, 9.0)))
